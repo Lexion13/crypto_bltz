7cda406c9c7c46d14b5133c866cef25f948c8131
Shoichiro Takahashi <lexion13@gmail.com>
2017-08-09 13:42:04 +0000

    fix

--- main.py
+++ main.py
@@ -7,23 +7,32 @@ import tornado.web
 import tornado.template
 
 from tornado.options import define, options
-define("port", default=8080, help="run on the given port", type=int)
 
+import pymongo
+from pymongo import MongoClient
+
+
+define("port", default=8080, help="run on the given port", type=int)
 
 class Application(tornado.web.Application):
     def __init__(self):
         handlers = [
             (r"/", IndexHandler),
             (r"/currency", CurrencyHandler),
+            (r"/articles", ArticleHandler),
             (r"/test", TestHandler),
+            (r"/mongotest", MongoTestHandler)
         ]
+        conn = MongoClient("localhost", 27017)
+        self.db = conn["definitions"]
         settings = dict(
             template_path=os.path.join(os.path.dirname(__file__), "templates"),
             static_path=os.path.join(os.path.dirname(__file__), "static"),
             ui_modules={
                 "Header": HeaderModule,
                 "Footer": FooterModule,
-                "Sidebar": SidebarModule
+                "Sidebar": SidebarModule,
+                "Currency": CurrencyModule
             },
             debug=True,
         )
@@ -41,12 +50,22 @@ class CurrencyHandler(tornado.web.RequestHandler):
     def get(self):
         title = self.get_argument('title', "fuck currency")
         name = self.get_argument('name', "name")
-        self.render('index.html')
+        self.render('currency.html',name=name, title=title)
+
+class ArticleHandler(tornado.web.RequestHandler):
+    def get(self):
+        title = self.get_argument('title', 'Articles page')
+        self.render('articles.html', title=title)
 
 class TestHandler(tornado.web.RequestHandler):
     def get(self):
         title = self.get_argument('title', 'test page')
         self.render('test.html', title=title)
+
+class MongoTestHandler(tornado.web.RequestHandler):
+    def get(self):
+        title = self.get_argument('title', "mongo test")
+        self.render('mongotest.html', title=title)
 
 class HeaderModule(tornado.web.UIModule):
     def render(self):
@@ -59,6 +78,13 @@ class FooterModule(tornado.web.UIModule):
 class SidebarModule(tornado.web.UIModule):
     def render(self):
         return self.render_string('modules/sidebar.html')
+
+
+class CurrencyModule(tornado.web.UIModule):
+    def render(self):
+        return self.render_string('modules/currency_module.html')
+
+
 
 def main():
     tornado.options.parse_command_line()
